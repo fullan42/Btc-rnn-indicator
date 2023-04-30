3450385d79fa03077384d141da411cb9e854d9db
fullan42 <hasan_-1@outlook.com>
2023-04-30 23:06:25 +0300

    first commit

---
+++ myModule/BitcoinPredictionModel.py
@@ -0,0 +1,86 @@
+import math
+import sklearn
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import tensorflow as tf
+from keras.callbacks import ModelCheckpoint, TensorBoard
+from keras.losses import mean_squared_error
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+import sklearn.metrics
+
+
+class BTCValuePredictor:
+
+    def __init__(self, csv_file_path):
+        self.csv_file_path = csv_file_path
+        self.dataset = pd.read_csv(csv_file_path)
+        self.dataset = self.dataset.iloc[:, 4:5]
+        self.sc = MinMaxScaler()
+        self.days_num = 30
+        self.checkpoint_path = 'model_checkpoint.h5'
+        self.tensorboard_callback = TensorBoard(log_dir='./logs', histogram_freq=1)
+
+    def prepare_data(self):
+        dataset_scaled = self.sc.fit_transform(self.dataset)
+        self.dataset_scaled = pd.DataFrame(dataset_scaled, columns=['Close'])
+        reshaped_data = np.zeros((len(self.dataset_scaled) - self.days_num - 1, self.days_num, 1))
+
+        for i in range(0, len(self.dataset_scaled) - self.days_num - 1):
+            reshaped_data[i] = self.dataset_scaled.iloc[i:i + self.days_num].values.reshape((self.days_num, 1))
+
+        num = int(len(reshaped_data) * 0.8)
+        train_set = reshaped_data[:num]
+        test_set = reshaped_data[num:]
+
+        self.xtrain = train_set[0:len(train_set)]
+        self.ytrain = self.dataset_scaled[self.days_num:len(train_set) + self.days_num]
+        self.xtest = test_set[0:len(test_set) - 1]
+        self.ytest = self.dataset_scaled[self.days_num + len(self.xtrain):]
+
+        self.ytrain = self.ytrain.to_numpy()
+        self.ytest = self.ytest.to_numpy()
+
+    def create_model(self):
+        self.model = Sequential()
+        self.model.add(LSTM(units=16, activation='sigmoid', input_shape=(self.days_num, 1)))
+        self.model.add(Dense(units=1))
+        self.model.compile(optimizer='adam', loss='mean_squared_error')
+
+    def train_model(self, batch_size=90, epochs=1000):
+        tf_callbacks = tf.keras.callbacks.TensorBoard(log_dir="logs/fit", histogram_freq=1)
+        checkpoint = ModelCheckpoint(self.checkpoint_path, monitor='val_loss', save_best_only=True, mode='min')
+        self.history = self.model.fit(self.xtrain, self.ytrain, batch_size=batch_size, epochs=epochs,
+                                      validation_split=0.2, callbacks=[checkpoint, tf_callbacks])
+
+    def predict(self):
+        predicted_close_price = self.model.predict(self.xtest)
+        predicted_close_price = self.sc.inverse_transform(predicted_close_price)
+        real_close_price = self.sc.inverse_transform(self.ytest)
+        self.predicted_close_price = predicted_close_price
+        self.real_close_price = real_close_price
+        mse = mean_squared_error(self.real_close_price, self.predicted_close_price)
+        rems = math.sqrt(mse)
+        predicted_direction = np.sign(np.diff(self.predicted_close_price))
+        actual_direction = np.sign(np.diff(self.real_close_price))
+        mda = np.mean(predicted_direction == actual_direction)
+        print("rems:"+rems+"mda:"+mda)
+
+    def plot_predictions(self):
+        plt.plot(self.real_close_price, color='red', label='Real BTC Value')
+        plt.plot(self.predicted_close_price, color='blue', label='Predicted BTC Value')
+        plt.title('BTC Value Prediction')
+        plt.xlabel('Index Num')
+        plt.ylabel('BTC Value')
+        plt.legend()
+        plt.show()
+
+    def plot_loss(self):
+        plt.plot(self.history.history['loss'])
+        plt.title('Loss per epoch')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend('train', loc='upper left')
+        plt.show()
---
+++ myModule/TestBTCPricePredictor.py
@@ -0,0 +1,23 @@
+from myModule.BitcoinPredictionModel import BTCValuePredictor
+from tensorboard import notebook
+
+
+class TestBTCPricePredictor:
+    btc_predictor = BTCValuePredictor('BTC-USD.csv')
+
+    btc_predictor.prepare_data()
+
+    btc_predictor.create_model()
+
+    btc_predictor.train_model(batch_size=64, epochs=50)
+
+    btc_predictor.predict()
+
+    btc_predictor.plot_predictions()
+
+    btc_predictor.plot_loss()
+
+    notebook.list()
+
+    # Start TensorBoard
+    notebook.start("--logdir logs")
